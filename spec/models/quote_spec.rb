cce3be77911bafd5545cf54717cd38b7e6362b4d
Roberto Barros <roberto.f.barros@gmail.com>
2025-07-30 14:53:35 -0300

    test quote attributes

--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Quote, type: :model do
+  let(:quote) { Quote.new(content: 'Keep moving forward', author: 'Walt Disney') }
+
   describe 'validations' do
     it { should validate_presence_of(:content) }
     it { should validate_presence_of(:author) }
+  end
+
+  describe 'attributes' do
+    it 'has content' do
+      expect(quote.content).to eq('Keep moving forward')
+    end
+
+    it 'has an author' do
+      expect(quote.author).to eq('Walt Disney')
+    end
   end
 end
